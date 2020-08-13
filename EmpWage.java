9362302106536fc2622db2cf45b030f2c9cc9bb7
kalyangoud145 <kalyangoud145@gmail.com>
2020-08-13 18:49:25 +0530

    Solving using Switch case

--- EmpWage.java
+++ EmpWage.java
@@ -5,25 +5,46 @@ public class EmpWage
       System.out.println("Welcome to Employee Wage Computation program");
 		int IS_FULLTIME=1,IS_PARTTIME=2,salary,empRatePerHr=20,empHrs;
       double empCheck=Math.floor(Math.random()*10)%3;
-      if (empCheck == IS_FULLTIME)
-        {
+		int empcheck=(int) empCheck;
+      	if (empcheck == IS_FULLTIME)
+        	{
 			empHrs=8;
    		salary=empRatePerHr*empHrs;
 			System.out.println("Present");
 			System.out.println("Salary: "+salary);
          }
-		else if (empCheck==IS_PARTTIME)
-		{
+			else if (empCheck==IS_PARTTIME)
+			{
 			empHrs=4;
          salary=empRatePerHr*empHrs;
          System.out.println("Present for parttime");
          System.out.println("Salary: "+salary);
-		}
-      else
+			}
+      	else
          {
          System.out.println("Absent");
 			System.out.println("Salary=0");
          }
+			System.out.println(empcheck);
+			switch (empcheck)
+			{
+			case 1:
+					empHrs=8;
+        	 		salary=empRatePerHr*empHrs;
+         		System.out.println("Present");
+         		System.out.println("Salary: "+salary);
+					break;
+			case 2:
+					empHrs=4;
+         		salary=empRatePerHr*empHrs;
+         		System.out.println("Present for parttime");
+         		System.out.println("Salary: "+salary);
+					break;
+			default:
+					System.out.println("Absent");
+         		System.out.println("Salary=0");
+					break;
+			}
    }
 }
 
