6e332d98cb76660a1ab58478b1463a6ccbcb4013
kalyangoud145 <kalyangoud145@gmail.com>
2020-08-13 16:57:17 +0530

    Adding Wage per Hr and N.o of working hours per day

--- EmpWage.java
+++ EmpWage.java
@@ -3,15 +3,20 @@ public class EmpWage
    public static void main(String [] args)
    {
       System.out.println("Welcome to Employee Wage Computation program");
-		int IS_FULLTIME=1;
+		int IS_FULLTIME=1,salary,empRatePerHr,empHrs;
       double empCheck=Math.floor(Math.random()*10)%2;
       if (empCheck == IS_FULLTIME)
          {
-         System.out.println("Present");
+			empRatePerHr=20;
+   		empHrs=8;
+   		salary=empRatePerHr*empHrs;
+			System.out.println("Present");
+			System.out.println("Salary: "+salary);
          }
       else
          {
          System.out.println("Absent");
+			System.out.println("Salary=0");
          }
    }
 }
