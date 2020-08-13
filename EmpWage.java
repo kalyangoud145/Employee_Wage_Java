38c473c40cc178d490efcbf899a1a6deac0742fd
kalyangoud145 <kalyangoud145@gmail.com>
2020-08-13 18:13:21 +0530

    Adding parttime

--- EmpWage.java
+++ EmpWage.java
@@ -3,16 +3,22 @@ public class EmpWage
    public static void main(String [] args)
    {
       System.out.println("Welcome to Employee Wage Computation program");
-		int IS_FULLTIME=1,salary,empRatePerHr,empHrs;
-      double empCheck=Math.floor(Math.random()*10)%2;
+		int IS_FULLTIME=1,IS_PARTTIME=2,salary,empRatePerHr=20,empHrs;
+      double empCheck=Math.floor(Math.random()*10)%3;
       if (empCheck == IS_FULLTIME)
-         {
-			empRatePerHr=20;
-   		empHrs=8;
+        {
+			empHrs=8;
    		salary=empRatePerHr*empHrs;
 			System.out.println("Present");
 			System.out.println("Salary: "+salary);
          }
+		else if (empCheck==IS_PARTTIME)
+		{
+			empHrs=4;
+         salary=empRatePerHr*empHrs;
+         System.out.println("Present for parttime");
+         System.out.println("Salary: "+salary);
+		}
       else
          {
          System.out.println("Absent");
