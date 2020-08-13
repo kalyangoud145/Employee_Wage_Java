ef275341c3bb377a54b97bca108efb83fd095226
kalyangoud145 <kalyangoud145@gmail.com>
2020-08-13 16:47:59 +0530

    Employee present or absent

--- EmpWage.java
+++ EmpWage.java
@@ -3,6 +3,16 @@ public class EmpWage
    public static void main(String [] args)
    {
       System.out.println("Welcome to Employee Wage Computation program");
+		int IS_FULLTIME=1;
+      double empCheck=Math.floor(Math.random()*10)%2;
+      if (empCheck == IS_FULLTIME)
+         {
+         System.out.println("Present");
+         }
+      else
+         {
+         System.out.println("Absent");
+         }
    }
 }
 
